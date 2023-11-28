9795f64dae40d9ade10a8067baa96ddcbd376158
programacion <programacion@192.168.56.1>
2023-11-28 19:17:01 +0100

    crude por consola con menu entero hecho

--- Tienda/src/main/java/com/tienda/accesodatos/DaoProductoSqlite.java
+++ Tienda/src/main/java/com/tienda/accesodatos/DaoProductoSqlite.java
@@ -16,7 +16,10 @@ public class DaoProductoSqlite implements DaoProducto {
 	private static final String SQL_SELECT = "SELECT id, nombre, precio, fecha_caducidad FROM productos";
 	private static final String SQL_SELECT_ID = SQL_SELECT + " WHERE id = ?";
 	private static final String SQL_INSERT = "INSERT INTO productos (nombre, precio, fecha_caducidad) VALUES (?,?,?)";
-
+	private static final String SQL_UPDATE = "UPDATE productos SET nombre=?,precio=?,fecha_caducidad=? WHERE id=?";
+	private static final String SQL_DELETE = "DELETE FROM productos WHERE id=?";
+	private static final String SQL_SELECT_NOMBRE = SQL_SELECT + " WHERE nombre LIKE ?";
+	
 	static {
 		try {
 			Class.forName("org.sqlite.JDBC");
@@ -105,21 +108,71 @@ public class DaoProductoSqlite implements DaoProducto {
 	}
 
 	@Override
-	public Productos modificar(Productos objeto) {
-		// TODO Auto-generated method stub
-		return null;
+	public Productos modificar(Productos producto) {
+		try (Connection con = obtenerConexion(); PreparedStatement pst = con.prepareStatement(SQL_UPDATE);) {
+			pst.setString(1, producto.getNombre());
+			pst.setBigDecimal(2, producto.getPrecio());
+
+			LocalDate fecha = producto.getFechaCaducidad();
+			String sFecha = fecha == null ? null : fecha.toString();
+
+			pst.setString(3, sFecha);
+			pst.setLong(4, producto.getId());
+
+			int numeroRegistrosModificados = pst.executeUpdate();
+
+			if (numeroRegistrosModificados != 1) {
+				throw new RuntimeException("La modificación ha devuelto " + numeroRegistrosModificados);
+			}
+
+			return producto;
+		} catch (SQLException e) {
+			throw new RuntimeException("No se han podido modificar el registro " + producto, e);
+		}
 	}
 
 	@Override
-	public void borrar(Long Id) {
-		// TODO Auto-generated method stub
+	public void borrar(Long id) {
+		try (Connection con = obtenerConexion(); PreparedStatement pst = con.prepareStatement(SQL_DELETE);) {
 
+			pst.setLong(1, id);
+
+			int numeroRegistrosModificados = pst.executeUpdate();
+
+			if (numeroRegistrosModificados != 1) {
+				throw new RuntimeException("El borrado ha devuelto " + numeroRegistrosModificados);
+			}
+
+		} catch (SQLException e) {
+			throw new RuntimeException("No se han podido borrar el registro " + id, e);
+		}
 	}
 
 	@Override
 	public Iterable<Productos> obtenerPorNombre(String nombreParcial) {
-		// TODO Auto-generated method stub
-		return null;
+		try (Connection con = obtenerConexion();
+				PreparedStatement pst = con.prepareStatement(SQL_SELECT_NOMBRE);
+				) {
+			pst.setString(1, "%" + nombreParcial + "%");
+			
+			ResultSet rs = pst.executeQuery();
+			var productos = new ArrayList<Productos>();
+			Productos producto;
+			LocalDate fecha;
+			String sFecha;
+			
+			while(rs.next()) {
+				sFecha = rs.getString("fecha_caducidad");
+				fecha = sFecha != null ? LocalDate.parse(sFecha) : null;
+				
+				producto = new Productos(rs.getLong("id"), rs.getString("nombre"), rs.getBigDecimal("precio"), fecha);
+				productos.add(producto);
+			}
+			
+			return productos;
+		} catch (SQLException e) {
+			throw new RuntimeException("No se han podido obtener registros con el nombre parcial " + nombreParcial, e);
+		}
 	}
 
 }
--- Tienda/src/main/java/com/tienda/presentacion/consola/DaoProductoSqlitePrueba.java
+++ Tienda/src/main/java/com/tienda/presentacion/consola/DaoProductoSqlitePrueba.java
@@ -1,5 +1,7 @@
 package com.tienda.presentacion.consola;
 
+import java.util.Scanner;
+
 import com.tienda.accesodatos.DaoProducto;
 import com.tienda.accesodatos.DaoProductoSqlite;
 import com.tienda.entidades.Productos;
@@ -8,16 +10,91 @@ public class DaoProductoSqlitePrueba {
 
 	public static void main(String[] args) {
 		DaoProducto dao = new DaoProductoSqlite();
+		Scanner scanner = new Scanner(System.in);
 
-//		dao.insertar(new Productos("Patatas"));
+		int opcion;
+		do {
+			System.out.println("Seleccione una opción:");
+			System.out.println("1. Ver todos los productos");
+			System.out.println("2. Modificar un producto");
+			System.out.println("3. Borrar un producto");
+			System.out.println("4. Buscar producto por el nombre");
+			System.out.println("5. Añadir un producto nuevo");
+			System.out.println("6. Salir");
+			System.out.print("Opción: ");
 
-		for (Productos p : dao.obtenerTodos()) {
-			System.out.println(p);
+			opcion = scanner.nextInt();
+			scanner.nextLine(); // Limpiar el buffer del Scanner
 
-		}
-			Productos producto = dao.obtenerPorId(2L);
-			System.out.println(producto + "es el producto con id = 2");
+			switch (opcion) {
+			case 1:
+				System.out.println("Listado de productos:");
+				for (Productos p : dao.obtenerTodos()) {
+					System.out.println(p);
+				}
+				break;
 
+			case 2:
+				System.out.print("Introduce el ID del producto a modificar: ");
+				long idModificar = scanner.nextLong();
+				scanner.nextLine(); // Limpiar el buffer del Scanner
+
+				Productos productoAModificar = dao.obtenerPorId(idModificar);
+				if (productoAModificar != null) {
+					System.out.print("Introduce el nuevo nombre: ");
+					String nuevoNombre = scanner.nextLine();
+					productoAModificar.setNombre(nuevoNombre);
+
+					dao.modificar(productoAModificar);
+					System.out.println("Producto modificado con éxito.");
+				} else {
+					System.out.println("No se encontró ningún producto con ese ID.");
+				}
+				break;
+
+			case 3:
+				System.out.print("Introduce el id a eliminar: ");
+				long idAEliminar = scanner.nextLong();
+				scanner.nextLine();
+
+				Productos productoAEliminar = dao.obtenerPorId(idAEliminar);
+				if (productoAEliminar != null) {
+					dao.borrar(idAEliminar);
+					System.out.println("Producto eliminado correctamente");
+				} else {
+					System.out.println("Error al eliminar el producto");
+				}
+				break;
+
+			case 4:
+				System.out.print("Introduce el nombre del producto: ");
+				String nombreProducto = scanner.nextLine();
+
+				// Obtener productos por nombre
+				Iterable<Productos> productosPorNombre = dao.obtenerPorNombre(nombreProducto);
+
+				System.out.println("Productos encontrados con el nombre '" + nombreProducto + "':");
+				for (Productos p : productosPorNombre) {
+					System.out.println(p);
+				}
+				break;
+
+			case 5:
+				System.out.print("Nombre del producto nuevo: ");
+				String nombreProductoNuevo = scanner.nextLine();
+				
+				dao.insertar(new  Productos(nombreProductoNuevo));
+				break;
+			case 6:
+				System.out.println("Saliendo del programa...");
+				break;
+			default:
+				System.out.println("Opción inválida, por favor seleccione una opción válida.");
+				break;
+
+			}
+		} while (opcion != 6);
+
+		scanner.close();
 	}
-
 }
